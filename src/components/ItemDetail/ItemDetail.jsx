4e0aa05dad809279194d8e2bcbf5becbc41c1909
tomas cardaci <tomascardaci99@gmail.com>
2023-05-09 08:50:19 -0300

    Proyecto-final-Tomás-Cardaci

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,19 +1,20 @@
-// import React, { useState } from 'react'
+import React, { useState } from 'react'
 import { Card } from 'react-bootstrap'
 
 import './ItemDetail.css'
 import ItemCount from '../ItemCount/ItemCount'
 import { useCartContext } from '../../context/CartContext'
+import { Link } from 'react-router-dom'
 
 const ItemDetail = ({product}) => {
 
-  // const [haveQuantity, setHaveQuantity] = useState(false)
+  const [haveQuantity, setHaveQuantity] = useState(false)
 
   const { addToCart } = useCartContext()
 
   const onAdd = (cantidad) => {
     addToCart({...product, quantity: cantidad})
-    // setHaveQuantity(true)
+    setHaveQuantity(true)
   }
 
   
@@ -30,7 +31,20 @@ const ItemDetail = ({product}) => {
                 <Card.Title><p className="CardTitle">{product.name}</p></Card.Title>
                 <Card.Text><p className="CardDescription">{product.description}</p></Card.Text>
               </Card.Body>
-              <ItemCount stock={product.stock} onAdd={onAdd}/>
+
+              <>
+              {haveQuantity ? 
+
+              <div className='ButtonLinksContainer'>
+                <Link className='ButtonLink' to='/'>Seguir comprando</Link>
+                <Link className='ButtonLink' to='/cart'>Terminar compra</Link>
+              </div>
+
+              :
+              <ItemCount stock={product.stock} onAdd={onAdd}/>}
+              </>
+              
+
 
             </div>
         
